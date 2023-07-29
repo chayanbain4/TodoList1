86c10fd0367a52655b195cf6ceba6116048f5a7e
Chayan Bain <140184783+chayanbain4@users.noreply.github.com>
2023-07-29 20:52:20 +0530

    Add files via upload

---
+++ New folder/src/app/MyComponents/todos/todos.component.ts
@@ -0,0 +1,54 @@
+import { Component, OnInit } from '@angular/core';
+import{ Todo } from "../../Todo";
+@Component({
+  selector: 'app-todos',
+  templateUrl: './todos.component.html',
+  styleUrls: ['./todos.component.css']
+})
+export class TodosComponent implements OnInit {
+
+
+
+  localItem: string|null;
+  todos!:Todo[];
+
+  constructor(){
+    this.localItem = localStorage.getItem("todos");
+    if(this.localItem == null){
+      this.todos=[];
+
+    }
+    else{
+      this.todos= JSON.parse(this.localItem);
+    }
+
+    
+  }
+
+  ngOnInit(): void { }
+
+
+  deleteTodo(todo:Todo){
+    console.log(todo);
+    const index = this.todos.indexOf(todo);
+    this.todos.splice(index, 1)
+    localStorage.setItem("todos",JSON.stringify(this.todos))
+  }
+
+  addTodo(todo:Todo){
+    console.log(todo);
+    this.todos.push(todo);
+    localStorage.setItem("todos",JSON.stringify(this.todos))
+
+  }
+  toggleTodo(todo:Todo){
+    const index = this.todos.indexOf(todo);
+    this.todos[index].active = !this.todos[index].active;
+    localStorage.setItem("todos",JSON.stringify(this.todos))
+
+  }
+
+
+
+
+}
